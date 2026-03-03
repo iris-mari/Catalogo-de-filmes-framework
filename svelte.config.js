62e8ed075ee5c41143ffd92fd13c351452f0444c
unknown <i32882329@gmail.com>
2026-03-02 21:57:45 -0300

    ajuste de build para netlify

--- svelte.config.js
+++ svelte.config.js
@@ -1,7 +1,12 @@
 import adapter from '@sveltejs/adapter-netlify';
+import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
-export default {
-  kit: {
-    adapter: adapter()
-  }
+/** @type {import('@sveltejs/kit').Config} */
+const config = {
+	preprocess: vitePreprocess(),
+	kit: {
+		adapter: adapter()
+	}
 };
+
+export default config;
